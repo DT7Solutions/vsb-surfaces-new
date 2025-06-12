0b7b76acc436236715e9527beca80f3872da73cd
Chaitanya Kothakota <120007815+chaitanyakothakota@users.noreply.github.com>
2025-06-12 16:13:18 +0530

    update the home page layout and images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,26 @@
-// Custom JavaScript for Sticky Header
+// Custom JavaScript for Products Slider
+const swiperNormal = new Swiper('.project-swiper-normal', {
+   loop: true,
+   speed: 4000,
+   slidesPerView: 'auto',
+   spaceBetween: 30,
+   autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+   },
+   freeMode: true,
+   freeModeMomentum: false,
+});
+const swiperReverse = new Swiper('.project-swiper-reverse', {
+   loop: true,
+   speed: 4000,
+   slidesPerView: 'auto',
+   spaceBetween: 30,
+   autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+      reverseDirection: true, // REVERSE AUTOPLAY
+   },
+   freeMode: true,
+   freeModeMomentum: false,
+});
